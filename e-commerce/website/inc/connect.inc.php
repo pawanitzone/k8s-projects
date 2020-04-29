37c6b7b180f5ed32fe845533226dc5a8e9ccd6b1
pawan Kumar <awspractice1999@gmail.com>
2020-04-29 13:18:43 +0530

    error correction

--- e-commerce/website/inc/connect.inc.php
+++ e-commerce/website/inc/connect.inc.php
@@ -1,4 +1,14 @@
-<?php 
-	mysql_connect("localhost","root","") or die("Couldn't connect to SQL server");
-	mysql_select_db("grocerydb") or die("Couldn'ttt select DB");
+<?php
+   $databaseHost = 'e-commerce-db-svc';
+   $databaseName = 'grocery';
+   $databaseUsername = 'pawank';
+   $databasePassword = '@pawan';
+
+   $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+   if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+//	mysql_connect("localhost","root","") or die("Couldn't connect to SQL server");
+//	mysql_select_db("grocerydb") or die("Couldn'ttt select DB");
 ?>
